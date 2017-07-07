b0adf82620c5bd5b6bca7d10307ffe82d49bf907
ubuntu16 <m.ramos@ortelio.co.uk>
2017-07-07 18:39:49 +0100

    tests wip

--- tests/asio_classes.cpp
+++ tests/asio_classes.cpp
@@ -1,29 +1,12 @@
-#define BOOST_TEST_MODULE ASIOTest
-/**
- * Copyright 2015 RAPP
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * #http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#define CATCH_CONFIG_MAIN
+#include <tests/Catch/single_include/catch.hpp>
+#include <noos/cloud/asio/http_response.hpp>
+#include <noos/cloud/asio/http_header.hpp>
+#include <noos/cloud/asio/http_post.hpp>
+#include <noos/cloud/asio/http_request.hpp>
+#include <noos/cloud/asio/platform.hpp>
+#include <noos/objects/picture.hpp>
 
-#include <boost/test/unit_test.hpp>
-#include <rapp/cloud/asio/http_response.hpp>
-#include <rapp/cloud/asio/http_header.hpp>
-#include <rapp/cloud/asio/http_post.hpp>
-#include <rapp/cloud/asio/http_request.hpp>
-#include <rapp/cloud/asio/platform.hpp>
-#include <rapp/objects/picture.hpp>
-
-BOOST_AUTO_TEST_SUITE(cloud_classes_test)
 
 /**
  * \brief TEST for http_reponse class
@@ -41,27 +24,33 @@ BOOST_AUTO_TEST_SUITE(cloud_classes_test)
  * Last, method 'has_content_lenght' is checked comparing with and size_t with
  * the same size that the header created before, and comparing them.
  */
-BOOST_AUTO_TEST_CASE(cloud_response_test)
+SCENARIO("Create a http_response", "[http_response]") 
 {
-    // header has zero content-length
-    std::string hardcoded_header = "GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1\r\n"; 
-                hardcoded_header += "Host: net.tutsplus.com\r\n";
-                hardcoded_header += "Accept-Encoding: gzip,deflate\r\n";
-                hardcoded_header += "Connection: keep-alive\r\n";
-                hardcoded_header += "Content-Length: 9\r\n";
-                hardcoded_header += "Cache-Control: no-cache\r\n\r\n";
+    GIVEN ("A std::string with the header data") {
+        // header has zero content-length
+        std::string hardcoded_header = "GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1\r\n"; 
+                    hardcoded_header += "Host: net.tutsplus.com\r\n";
+                    hardcoded_header += "Accept-Encoding: gzip,deflate\r\n";
+                    hardcoded_header += "Connection: keep-alive\r\n";
+                    hardcoded_header += "Content-Length: 9\r\n";
+                    hardcoded_header += "Cache-Control: no-cache\r\n\r\n";
 
-    std::string buf = hardcoded_header;
-    auto obj1 = std::make_unique<rapp::cloud::http_response>(buf);
-
-    BOOST_CHECK(obj1);
-    BOOST_CHECK_EQUAL(9, obj1->content_length());
-
-    auto obj2 = std::make_unique<rapp::cloud::http_response>(buf);
-    BOOST_CHECK(obj2);
-    BOOST_CHECK_EQUAL(9, obj2->content_length());
-
-    //BOOST_CHECK_EQUAL(*obj2, *obj1);
+        std::string buf = hardcoded_header;
+        WHEN ("http_response is constructed") {
+            auto obj1 = std::make_unique<noos::cloud::http_response>(buf);
+            THEN ("the content lenght should be the same as the given header") {
+                REQUIRE(obj1);
+                REQUIRE(9 == obj1->content_length());
+            }
+        }
+        WHEN ("http_response is copied") { 
+            auto obj2 = std::make_unique<noos::cloud::http_response>(buf);
+            THEN ("the data should be the same for both") { 
+                REQUIRE(obj2);
+                REQUIRE(9 == obj2->content_length());
+            }
+        }
+    }
 }
 
 /**
@@ -73,26 +62,35 @@ BOOST_AUTO_TEST_CASE(cloud_response_test)
  * Second, method 'get boundary' is checked comparing the attibute of the first
  * object with itself
  */
-BOOST_AUTO_TEST_CASE(cloud_http_header_test)
+SCENARIO("Create http_header", "[http_header]")
 {
-    std::string ur = "hop/test\r\n";
-    rapp::cloud::http_header head1 = rapp::cloud::http_header(ur);
-
-    std::string head2 = "hop/test\r\n";
-                head2 += "Host: http://example:8080\r\n";
-                head2 += "User-Token: \r\n";
-                head2 += "Accept-Token: token\r\n";
-                head2 += "Content-Length: 8\r\n";
-                head2 += "\r\n\r\n";
+    GIVEN ("A known uri(std::string)") {
+        std::string ur = "hop/test\r\n";
+        noos::cloud::http_header head1(ur);
+        WHEN("http_header is constructed") {
+            THEN("The object is created") {
+                REQUIRE(!head1.get_boundary().empty());
+            }
+        }
+        WHEN("http_header is given the platform data") {
+            THEN("All the attributes are filled") {
+                std::string head2 = "hop/test\r\n";
+                            head2 += "Host: http://example:8080\r\n";
+                            head2 += "User-Token: \r\n";
+                            head2 += "Accept-Token: token\r\n";
+                            head2 += "Content-Length: 8\r\n";
+                            head2 += "\r\n\r\n";
     
-    rapp::cloud::platform info = { "http://example", "8080", "token"};
-    unsigned int length_test = 8;
-    std::string head3 = head1.to_string(info, length_test);
-   
-    BOOST_CHECK_EQUAL(head3, head2);                
+                noos::cloud::platform info = { "http://example", "8080", "token"};
+                unsigned int length_test = 8;
+                std::string head3 = head1.to_string(info, length_test);
+                REQUIRE(head3 == head2);
 
-    std::string test2 = head1.get_boundary();
-    BOOST_CHECK_EQUAL(head1.get_boundary(), test2);
+                std::string boundary_test = head1.get_boundary();
+                REQUIRE(head1.get_boundary() == boundary_test);
+            }
+        }
+    }
 }
 
 /**
@@ -109,77 +107,87 @@ BOOST_AUTO_TEST_CASE(cloud_http_header_test)
  * Last, method 'size' is checked using the same string and calculating
  * the size manually and with the method.
  */ 
-BOOST_AUTO_TEST_CASE(cloud_http_post_test)
+SCENARIO("Create http_post", "[http_post]")
 {
-    std::string boundary_example = rapp::misc::random_boundary();
-    auto post1 = std::unique_ptr<rapp::cloud::http_post>(new rapp::cloud::http_post(boundary_example)); 
-    BOOST_CHECK(post1);
+    GIVEN("http_post is constructed") {
+        std::string boundary_example = noos::misc::random_boundary();
+        auto post1 = std::unique_ptr<noos::cloud::http_post>(new noos::cloud::http_post(boundary_example)); 
+        REQUIRE(post1);
 
-    auto post2 = std::unique_ptr<rapp::cloud::http_post>(new rapp::cloud::http_post(boundary_example));
-    BOOST_CHECK(post2);
+        WHEN("A second http_post is constructed with the same boundary") {
+            auto post2 = std::unique_ptr<noos::cloud::http_post>(new noos::cloud::http_post(boundary_example));
+            THEN("Both objects are equals") {
+                REQUIRE(post2);
+                REQUIRE(post1->to_string() == post2->to_string());
+            }
+        }
 
-    // check if two http posts with same boundary are the same
-    BOOST_CHECK_EQUAL(post1->to_string(), post2->to_string());
-    
-    // craft a manual POST Content Disposition and compare it to the method
-    std::string string_post3 = post1->to_string()
-                             + "--" + boundary_example + "\r\n"
-                             + "Content-Disposition: form-data; name=\"blah\"\r\n\r\n"
-                             + "blah blah blah"
-                             + "\r\n"; 
+        WHEN("Add_content method is called") {
+            THEN("data attribute is filled(no image)") {
+                // craft a manual POST Content Disposition and compare it to the method
+                std::string string_post3 = post1->to_string()
+                                         + "--" + boundary_example + "\r\n"
+                                         + "Content-Disposition: form-data; name=\"blah\"\r\n\r\n"
+                                         + "blah blah blah"
+                                         + "\r\n"; 
 
-    auto post4 = std::unique_ptr<rapp::cloud::http_post>(new rapp::cloud::http_post(boundary_example));
-    BOOST_CHECK(post4);
-    post4->add_content("blah", "blah blah blah", true);
+                auto post4 = std::unique_ptr<noos::cloud::http_post>(new noos::cloud::http_post(boundary_example));
+                REQUIRE(post4);
+                post4->add_content("blah", "blah blah blah", true);
+                // compare manual string with method one
+                REQUIRE(post4->to_string() == string_post3);
+            }
+            THEN("data attribute is filled(with an image)") {
+                // load the picture bytes
+                const std::string file = "tests/data/asio_classes_qr_code_1.png";
+                auto pic = std::unique_ptr<noos::object::picture>(new noos::object::picture(file));
+                REQUIRE(pic);
+                const auto bytes = pic->bytearray();
+                std::string string_post4 = "--" + boundary_example + "\r\n"
+                                         + "Content-Disposition: form-data; name=\"blah\"\r\n"
+                                         + "Content-Type: application/octet-stream\r\n\r\n";
+                string_post4.insert(string_post4.end(), bytes.begin(), bytes.end());
+                string_post4 += "\r\n";                     
 
-    // compare manual string with method one
-    BOOST_CHECK_EQUAL(post4->to_string(), string_post3);
-
-    // load the picture byes
-    const std::string file = "tests/data/asio_classes_qr_code_1.png";
-    auto pic = std::unique_ptr<rapp::object::picture>(new rapp::object::picture(file));
-    BOOST_CHECK(pic);
-    const auto bytes = pic->bytearray();
-    std::string string_post4 = "--" + boundary_example + "\r\n"
-                             + "Content-Disposition: form-data; name=\"blah\"\r\n"
-                             //+ " filename=\"" + file + "\"\r\n\r\n";
-                             + "Content-Type: application/octet-stream\r\n\r\n";
-    string_post4.insert(string_post4.end(), bytes.begin(), bytes.end());
-    string_post4 += "\r\n";                     
-
-    auto post5 = std::unique_ptr<rapp::cloud::http_post>(new rapp::cloud::http_post(boundary_example));
-    BOOST_CHECK(post5);
-    post5->add_content("blah", file, bytes);
-
-    // test the content dispoition for files with manual string
-    BOOST_CHECK_EQUAL(post5->to_string().size(), string_post4.size());
-
-    // check post sizes
-    unsigned int str_size = string_post4.size()*sizeof(std::string::value_type);
-    BOOST_CHECK_EQUAL(str_size, post5->size());
-
-    // check post sizes
-    BOOST_CHECK(post4->size() != post5->size()); 
+                auto post5 = std::unique_ptr<noos::cloud::http_post>(new noos::cloud::http_post(boundary_example));
+                REQUIRE(post5);
+                post5->add_content("blah", file, bytes);
+                // test the content disposition for files with manual string
+                REQUIRE(post5->to_string().size() == string_post4.size());
+                // check post sizes
+                unsigned int str_size = string_post4.size()*sizeof(std::string::value_type);
+                REQUIRE(str_size == post5->size());
+            }
+        }
+    }
 }
 
 /**
- *\brief TEST for http_request class
- *
- * This class was checked looking with `cout` command what was the result 
+ * \brief TEST for http_request class
  */
-BOOST_AUTO_TEST_CASE(cloud_request_test)
+SCENARIO("http_request", "[http_request]")
 {
-    std::string boundary1 = rapp::misc::random_boundary();
-    const std::string ur = "ur\r\n";
-    std::string ur2 = "ur\r\n";
+    GIVEN("A known uri") {
+        const std::string ur = "ur\r\n";
+        std::string ur2 = ur;
+        THEN("http_request is constructed and create copies") {
+            auto request_test1 = noos::cloud::http_request(ur);
+            auto request_test2 = noos::cloud::http_request(request_test1);
+            REQUIRE(request_test1 == request_test2);
 
-    auto request_test = rapp::cloud::http_request(ur);
-    auto request_2 = std::unique_ptr<rapp::cloud::http_request>(new rapp::cloud::http_request(ur2));
-    BOOST_CHECK(request_2);
-
-    rapp::cloud::platform info = {"localhost", "9001", "rapp_token"};
-    boost::asio::streambuf buffer;
-    request_test.fill_buffer(buffer, info); 
+            auto request_test3 = noos::cloud::http_request(ur, false);
+            auto request_test4 = noos::cloud::http_request(request_test3);
+            REQUIRE(request_test3 == request_test4);
+        }
+        THEN("two different object constructed with the same uri are not equal") {
+            auto request_test5 = noos::cloud::http_request(ur);
+            auto request_test6 = noos::cloud::http_request(ur);
+            REQUIRE((request_test5 == request_test6) == false);
+        }
+    }
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+/**
+ * \brief TEST for platform
+ */
+SCENARIO()
