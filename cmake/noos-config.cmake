69f7cc32f5b84e3615c6beb73f3b074a1c6d18dc
Maria Ramos <m.ramos@ortelio.co.uk>
2017-08-08 15:13:41 +0100

    CMakeLists.txt modified

--- cmake/noos-config.cmake
+++ cmake/noos-config.cmake
@@ -1,24 +1,24 @@
-# - Config file for the RAPP package
+# - Config file for the NOOS package
 # It defines the following variables
-#  RAPP_INCLUDE_DIRS - include directories for RAPP
-#  RAPP_LIBRARIES    - libraries to link against
-#  RAPP_STATIC_LIBRARIES - static libraries to link against
+#  NOOS_INCLUDE_DIRS - include directories for NOOS
+#  NOOS_LIBRARIES    - libraries to link against
+#  NOOS_STATIC_LIBRARIES - static libraries to link against
 
-if(EXISTS ${CMAKE_INSTALL_PREFIX}/lib/librapp.a) 
-  set(RAPP_STATIC_LIBRARIES
-      ${CMAKE_INSTALL_PREFIX}/lib/librapp.a
+if(EXISTS ${CMAKE_INSTALL_PREFIX}/lib/libnoos.a) 
+  set(NOOS_STATIC_LIBRARIES
+      ${CMAKE_INSTALL_PREFIX}/lib/libnoos.a
       CACHE INTERNAL "" FORCE
   )
 endif()
 
-if(EXISTS ${CMAKE_INSTALL_PREFIX}/lib/librapp.so)
-  set(RAPP_LIBRARIES
-      ${CMAKE_INSTALL_PREFIX}/lib/librapp.so
+if(EXISTS ${CMAKE_INSTALL_PREFIX}/lib/libnoos.so)
+  set(NOOS_LIBRARIES
+      ${CMAKE_INSTALL_PREFIX}/lib/libnoos.so
       CACHE INTERNAL "" FORCE
   )
 endif()
 
-set(RAPP_INCLUDE_DIRS
+set(NOOS_INCLUDE_DIRS
     ${CMAKE_INSTALL_PREFIX}/include
     CACHE INTERNAL "" FORCE
 )
